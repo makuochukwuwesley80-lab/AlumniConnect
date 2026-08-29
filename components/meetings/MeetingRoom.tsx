62038a98477dba4ddc2ca207287af2cd060e6b62
Your Name <your@email.com>
2026-08-29 19:06:21 +0100

    fix MeetingRoom email prop

--- components/meetings/MeetingRoom.tsx
+++ components/meetings/MeetingRoom.tsx
@@ -3,37 +3,40 @@
 import { JitsiMeeting } from "@jitsi/react-sdk";
 
 type MeetingRoomProps = {
-  roomName: string;
-  displayName: string;
+roomName: string;
+displayName: string;
+email: string;
 };
 
 export function MeetingRoom({
-  roomName,
-  displayName,
+roomName,
+displayName,
+email,
 }: MeetingRoomProps) {
-  return (
-    <div className="overflow-hidden rounded-[32px] border border-white/10 bg-white/5 shadow-2xl backdrop-blur-3xl">
-      <JitsiMeeting
-        domain="meet.jit.si"
-        roomName={roomName}
-        configOverwrite={{
-          startWithAudioMuted: false,
-          startWithVideoMuted: false,
-          prejoinPageEnabled: true,
-        }}
-        interfaceConfigOverwrite={{
-          SHOW_JITSI_WATERMARK: false,
-          SHOW_WATERMARK_FOR_GUESTS: false,
-        }}
-        userInfo={{
-          displayName,
-        }}
-        getIFrameRef={(iframe) => {
-          iframe.style.height = "85vh";
-          iframe.style.width = "100%";
-          iframe.style.border = "0";
-        }}
-      />
-    </div>
-  );
+return (
+<div className="overflow-hidden rounded-[32px] border border-white/10 bg-white/5 shadow-2xl backdrop-blur-3xl">
+<JitsiMeeting
+domain="meet.jit.si"
+roomName={roomName}
+configOverwrite={{
+startWithAudioMuted: false,
+startWithVideoMuted: false,
+prejoinPageEnabled: true,
+}}
+interfaceConfigOverwrite={{
+SHOW_JITSI_WATERMARK: false,
+SHOW_WATERMARK_FOR_GUESTS: false,
+}}
+userInfo={{
+displayName,
+email,
+}}
+getIFrameRef={(iframe) => {
+iframe.style.height = "85vh";
+iframe.style.width = "100%";
+iframe.style.border = "0";
+}}
+/>
+</div>
+);
 }
